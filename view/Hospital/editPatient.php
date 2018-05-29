a86c4b1931dfcb817d84d9eba4ddcd5e193cceae
Kiran Kovacic <99048377@mydavinci.nl>
2018-05-29 08:38:55 +0200

    edit patient is aangepast, nu krijg je de juste patient en specie in de selectie balk en de juste gender aan gevinkt.

--- view/Hospital/editPatient.php
+++ view/Hospital/editPatient.php
@@ -8,18 +8,18 @@
     <p>Name of the owner</p>
     <select required name="client_id">
       <?php foreach ($clients as $client) { ?>
-        <option value="<?= $client['client_id'] ?>"><?= $client['client_firstname'] . "&nbsp;" . $client['client_lastname'] ?></option>
+        <option value="<?= $client['client_id'] ?>" <?php if ($patient['client_id'] == $client['client_id']) { ?> selected = true <?php } ?>><?= $client['client_firstname'] . "&nbsp;" . $client['client_lastname'] ?></option>
       <?php } ?>
     </select>
 
     <p>Gender of the pet</p>
-    <input type="radio" name="gender_id" value="1"><span>Male</span><br>
-    <input type="radio" name="gender_id" value="2"><span>Female</span>
+      <input type="radio" name="gender_id" value="1" <?php if ($patient['gender_id'] == 1) {?> checked <?php } ?>><span>Male</span><br>
+      <input type="radio" name="gender_id" value="2" <?php if ($patient['gender_id'] == 2) {?> checked <?php } ?>><span>Female</span>
 
     <p>specie</p>
     <select required name="specie_id">
       <?php foreach ($species as $specie) { ?>
-        <option value="<?= $specie['species_id'] ?>"><?= $specie['species_description'] ?></option>
+        <option value="<?= $specie['species_id'] ?>" <?php if ($patient['species_id'] == $specie['species_id']) { ?> selected = true <?php } ?>><?= $specie['species_description'] ?></option>
       <?php } ?>
     </select>
     <p>Status</p>
